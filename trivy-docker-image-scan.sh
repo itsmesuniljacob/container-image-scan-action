1fd9e7bc781ee5a79bd50b61827f2ed77e417070
Sunil-Jacob <89004489+Sunil-Jacob@users.noreply.github.com>
2022-01-21 10:12:37 +0530

    Update trivy image version

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -2,7 +2,7 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+docker run --rm -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.22.0 -q image --exit-code 0 --severity CRITICAL --light $dockerImageName
 
 # Trivy scan result processing
 exit_code=$?
